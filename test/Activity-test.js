43c6dcd44623a63bfa12ebe87b3d0c269af2f04e
danielsolomon332 <daniel.solomon332@gmail.com>
2022-02-23 18:00:30 -0700

    Set up beforeEach in Activity Test

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -4,3 +4,19 @@ import userTestData from '../src/data/user-test-data';
 import activityTestData from '../src/data/activity-test-data';
 import User from '../src/js/User';
 import Activity from '../src/js/Activity';
+
+describe('Activity', () => {
+  let userRepository;
+  let user1;
+  let user2;
+  let activity;
+
+  beforeEach(() => {
+    userRepository = new UserRepository(userTestData)
+    user1 = new User(userTestData[0]);
+    user2 = new User(userTestData[1]);
+    user1.activity = new Activity(activityTestData, user1.id);
+    user2.activity = new Activity(activityTestData, user2.id);
+  });
+
+})
