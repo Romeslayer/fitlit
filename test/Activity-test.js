5205e210fa172d62a0e2fe298081750f70d1b6cf
danielsolomon332 <daniel.solomon332@gmail.com>
2022-02-23 17:57:30 -0700

    Set up Activity Test file

---
+++ test/Activity-test.js
@@ -0,0 +1,6 @@
+import { expect } from 'chai';
+import UserRepository from '../src/js/UserRepository';
+import userTestData from '../src/data/user-test-data';
+import activityTestData from '../src/data/activity-test-data';
+import User from '../src/js/User';
+import Activity from '../src/js/Activity';
