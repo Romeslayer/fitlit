2453de0fc2ee7d1e06581b617f597701ba0e84d5
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-02-26 11:28:03 -0700

    Create post function and export getData and postData functions

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,6 +4,19 @@ const getData = (fetchAPI) => {
     .then(response => response.json());
 }
 
+const postData = (fetchAPI, formData) => {
+  return fetch(`http://localhost:3001/api/v1/${fetchAPI}`,
+    {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(formData)
+  })
+  .then(response => {
+    if !response.ok throw new Error('Please fill out all fields.')
+    return reponse.json()
+  })
+  .catch(e => console.log(e))
+}
 const usersData = getData('users');
 const sleepData = getData('sleep');
 const activityData = getData('activity');
@@ -13,5 +26,7 @@ export {
   usersData,
   sleepData,
   activityData,
-  hydrationData
+  hydrationData,
+  getData,
+  postData
 };
