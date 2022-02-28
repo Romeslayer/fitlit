b9b7afe971ea28c01c340b853bcae03c7af02aa9
danielsolomon332 <daniel.solomon332@gmail.com>
2022-02-28 14:10:14 -0700

    Add background text for charts for accessibility

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -6,7 +6,7 @@ const displayButtons = document.querySelector('.form-display-buttons');
 let updateDOM = (currentUser, users) => {
   updateUser(currentUser, users);
   makeCharts(currentUser);
-
+  hideForms();
   displayStats(currentUser);
 }
 
@@ -62,8 +62,9 @@ const updateUser = (currentUser, users) => {
 
 const displayStats = (currentUser) => {
   todaysIntake.querySelector('canvas').innerHTML +=
-  `<p>Your water intake for today is: ${currentUser.hydration.getDaily(currentUser.hydration.days[currentUser.hydration.days.length - 1].date)} fl oz`;
-  currentUser.hydration.getWeekly().forEach(day => weeklyIntake.querySelector('canvas').innerHTML += `<p>On ${day.date} you drank: ${day.numOunces} fl oz</p>`);
+  `<p>Your water intake for today is: ${currentUser.hydration.getDaily(currentUser.hydration.days[currentUser.hydration.days.length - 1].date)} fl oz, compared to your record intake which was: ${hydration.days.map(day => day.numOunces).sort((a,b) => b - a)[0]}`;
+  currentUser.hydration.getWeekly().forEach(day => weeklyIntake.querySelector('canvas').innerHTML +=
+  `<p>On ${day.date} you drank: ${day.numOunces} fl oz</p>`);
   avgSleepHours.querySelector('canvas').innerHTML +=
   `<p>Your total sleep hour average is: ${currentUser.sleep.getAverage()}`;
   avgSleepQuality.querySelector('canvas').innerHTML +=
@@ -72,9 +73,29 @@ const displayStats = (currentUser) => {
   `<p>You slept ${currentUser.sleep.getSleep(currentUser.sleep.days[currentUser.sleep.days.length - 1].date)} hours last night.`;
   todaysSleepQuality.querySelector('canvas').innerHTML +=
   `<p>Your sleep quality last night was: ${currentUser.sleep.getSleepQuality(currentUser.sleep.days[currentUser.sleep.days.length - 1].date)}.`;
-  currentUser.sleep.getWeekSleep(currentUser.sleep.days[currentUser.sleep.days.length - 8].date).forEach(day => weeklySleepHours.querySelector('canvas').innerHTML += `<p>On ${day.date}, you slept ${day.hoursSlept} hours.`);
-  currentUser.sleep.getWeekQuality(currentUser.sleep.days[currentUser.sleep.days.length - 8].date).forEach(day => weeklySleepQuality.querySelector('canvas').innerHTML += `<p>On ${day.date}, your sleep quality was: ${day.sleepQuality}.`);
-}
+  currentUser.sleep.getWeekSleep(currentUser.sleep.days[currentUser.sleep.days.length - 8].date).forEach(day => weeklySleepHours.querySelector('canvas').innerHTML +=
+  `<p>On ${day.date}, you slept ${day.hoursSlept} hours.`);
+  currentUser.sleep.getWeekQuality(currentUser.sleep.days[currentUser.sleep.days.length - 8].date).forEach(day => weeklySleepQuality.querySelector('canvas').innerHTML +=
+  `<p>On ${day.date}, your sleep quality was: ${day.sleepQuality}.`);
+  todaysNumOfSteps.querySelector('canvas').innerHTML +=
+  `<p>On ${day.date} you walked ${currentUser.activity.days[currentUser.activity.days.length - 1].numSteps} number of steps today. Your step goal is ${currentUser.dailyStepGoal}</p>`;
+  todaysMinActive.querySelector('canvas').innerHTML +=
+  `<p>On ${day.date} you were active for ${currentUser.activity.days[activity.days.length - 1].minutesActive}. Your personal record is ${currentUser.activity.days.map(day => day.minutesActive).sort((a,b) => b - a)[0]}</p>`;
+  todaysMilesWalked.querySelector('canvas').innerHTML +=
+  `<p>On ${day.date} you walked ${currentUser.activity.getMiles(currentUser.activity.days[activity.days.length - 1].date)} miles. Your personal record is ${currentUser.activity.days.map(day => day).sort((a,b) => b.numSteps - a.numSteps)[0]}</p>`;
+  comparedSteps.querySelector('canvas').innerHTML +=
+  `<p>On ${day.date} you walked ${currentUser.activity.days[activity.days.length - 1].numSteps} steps. The Fitlit average was ${currentUser.activity.allUsersStepsAvg(currentUser.activity.days[activity.days.length - 1].date)}</p>`;
+  comparedMinActive.querySelector('canvas').innerHTML +=
+  `<p>On ${day.date} you were active for ${currentUser.activity.days[activity.days.length - 1].minutesActive}. The Fitlit average was ${currentUser.activity.allUsersMinutesAvg(currentUser.activity.days[activity.days.length - 1].date)}</p>`;
+  comparedFlights.querySelector('canvas').innerHTML +=
+  `<p>On ${day.date} you climbed ${currentUser.activity.days[activity.days.length - 1].flightsOfStairs} flights of stairs. The Fitlit average was ${currentUser.activity.allUsersStairsAvg(currentUser.activity.days[activity.days.length - 1].date)}</p>`;
+  currentUser.activity.days.slice(-7).forEach(day => weeklySteps.querySelector('canvas').innerHTML +=
+  `<p>On ${day.date} you walked ${day.numSteps} steps.</p>`);
+  currentUser.activity.days.slice(-7).forEach(day => weeklyFlights.querySelector('canvas').innerHTML +=
+  `<p>On ${day.date} you climbed ${day.flightsOfStairs} flights of stairs.</p>`);
+  currentUser.activity.days.slice(-7).forEach(day => weeklyMinActive.querySelector('canvas').innerHTML +=
+  `<p>On ${day.date} you were active for ${day.minutesActive} minutes.</p>`);
+};
 
 const hideForms = () => {
   activityForm.classList = 'activity-form hidden';
